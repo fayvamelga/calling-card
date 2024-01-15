4b8df50f87a1623508eb33138df66371f7f72917
Fayven <fayven.amelga@gmail.com>
2024-01-15 16:49:20 -0500

    Changed projects text to white

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -8,6 +8,7 @@ function About() {
   return (
     <>
     <div id="About" className="div"></div>
+    <br></br>
     <h1 className="about-title" >About Us</h1>
       <Container className="about">
         {/* <Row className="about-spacing"> */}
@@ -17,12 +18,12 @@ function About() {
       <img src={wwa}  alt="Calling Card logo" /> 
       </Row>
         <Row className="about">
-            <h3 className="about-title">Who We Are</h3>
+            <h1 className="about-title">Who We Are</h1>
         </Row>
 
         <Row className="about">
           <li>
-<ul className="about-text">Calling Card LLC is a media company that develops and shares Ethiopian stories from across the globe.</ul>
+<h2 className="about-text">Calling Card LLC is a media company that develops and shares Ethiopian stories from across the globe.</h2>
 </li>
         </Row>
         </Card>
@@ -34,68 +35,24 @@ function About() {
       <img src={wwd}  alt="Calling Card logo" /> 
       </Row >
  <Row className="about">
-     <h3 className="about-title">What We Do</h3>
+     <h1 className="about-title">What We Do</h1>
  </Row>
 
  <Row>
   <li>
-<ul className="about-text">Podcasting</ul>
-<ul className="about-text">Screenwriting</ul>
-<ul className="about-text">Film Production</ul>
+<h2 className="about-text">Podcasting</h2>
+<h2 className="about-text">Screenwriting</h2>
+<h2 className="about-text">Film Production</h2>
 </li>
  </Row>
  </Card>
         </Col>
-    {/* <Col>
-    <Card className="border">
- 
-      <Row className="about">
-          <h2 className="about-title">Mission</h2>
-      </Row>
-
-      <Row className="about-text">
-<h3>Our mission is to magnify Ethiopian voices from around the world.</h3>
-      </Row>
-      </Card>
-      </Col> */}
-      {/* </Row> */}
-
-      {/* <Row>
-    <Col>
-    <Card className="border">
- 
-      <Row className="about">
-          <h2 className="about-title">Our Objectives</h2>
-      </Row>
-
-      <Row >
-      <ul >
-<h3 className="about-text">Produce media that speaks to the Ethiopian experience.</h3>
-<h3 className="about-text">Produce tools, resources, and opportunities for Ethiopians to share their stories.</h3>
-</ul>
-      </Row>
-      </Card>
-      </Col>
-    <Col>
-    
-    <Card className="border">
- 
-      <Row className="about">
-          <h2 className="about-title">What We Do</h2>
-      </Row>
-
-      <Row>
-        <ul >
-<h3 className="about-text">Podcasting</h3>
-<h3 className="about-text">Screenwriting</h3>
-<h3 className="about-text">Film Production</h3>
-</ul>
-      </Row>
-      </Card>
-      </Col>
-      </Row> */}
+  
         </Container>
-        
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
 
     </>
   );
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,44 +1,47 @@
 import React from "react";
-import { Row, Container, Card, Col, Image } from "react-bootstrap";
-import ceremony from '../images/ceremony.png'
-import podcast from '../images/podcast.png'
+import { Row, Container, Col, Image } from "react-bootstrap";
+import ceremony from '../images/ceremony.png';
+import podcast from '../images/podcast.png';
 
 
 function Projects() {
   return (
     <>
+    
     <div id="Projects" className="projDiv"></div>
     <h1 className="about-title" >Projects</h1>
       <Container className="about">
-        <Row className="about-spacing">
-    <Col className="about-spacing">
-      <Card className="projBorder">
-   
-        <Row className="about">
-            <h2 className="about-title">Ceremony</h2>
-        </Row>
-        <Row>
-            <Image className="ceremonyImg" src={ceremony}>
-            </Image>
-        </Row>
-        <Row className="about-text">
-<h3>Ceremony is a short film that 
-showcases an encounter 
-between two strangers over 
-traditional Ethiopian coffee.
-</h3>
-        </Row>
-        </Card>
-        </Col>
+        {/* <Row className="about-spacing"> */}
+    {/* <Col className="about-spacing"> */}
+    <Row >
+    <h2 className="about-title">Ceremony</h2>
+    <br></br>
+    <Row>
+      <Col >
+        <Image className="ceremonyImg" src={ceremony} alt="Ceremony Image" />
+      </Col>
+      <Col >
+        <p className="ceremonyText">
+          Ceremony is a short film that showcases an encounter between two strangers over traditional Ethiopian coffee.
+        </p>
+      </Col>
+    </Row>
+</Row>
+
+        {/* </Col> */}
+
+
+
     <Col >
-    <Card className="projBorder">
+    
  
       <Row className="about">
           <h2 className="about-title">Start From the Beginning</h2>
       </Row>
   
-      <Row className="about-text">
-<h3>Through a series of interview 
+  <Row>
+      <Col >
+<h3 className="ceremonyText">Through a series of interview 
 conversations, Start from 
 the Beginning explores the 
 journeys of an Ethiopian 
@@ -47,14 +50,15 @@ US, how they got here, what
 they found when they 
 arrived, and why each of them 
 decided to stayed, or didn’t.</h3>
-      </Row>
-      <Row>
+      </Col>
+      <Col>
             <Image className="podcastImg" src={podcast}>
             </Image>
+        </Col>
         </Row>
-      </Card>
+      {/* </Card> */}
       </Col>
-      </Row>
+      {/* </Row> */}
         </Container>
         
 
