b793f4ed1c0027ea8499a8c83b7143bbedab7d8a
Fayven <fayven.amelga@gmail.com>
2024-01-15 21:29:42 -0500

    Update podcast page bootstrap

--- client/src/pages/Podcast.js
+++ client/src/pages/Podcast.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Row, Container, Card, Col } from "react-bootstrap";
+import { Row, Container, Image, Col } from "react-bootstrap";
+import podcast from "../images/podcast.png";
 
 function Podcast () {
     return (
@@ -7,10 +8,24 @@ function Podcast () {
         <div id="Podcast" className="div">
             <br></br>
             <h1 className="about-title">Podcast</h1>
-        <Container>
-            <Row className="contact">
-                <h2 className="contactText">There are currently no episodes. Check back soon!</h2>
+            <br></br>
+        <Container className="text-center">
+            <Row >
+                <h2 className="ceremonyText">There are currently no episodes. Check back soon!</h2>
             </Row>
+            <br></br>
+            <br></br>
+            <Row  >
+                <Col md={{ span: 6, offset: 3}}>
+            <Image
+            className="podcastImg" 
+              src={podcast}
+              alt="podcast Image"
+            />
+            </Col>
+            </Row>
+            <br></br>
+            <br></br>
         </Container>
         </div>
         </>
